07dd82e63e20a2538fa91f0cdc825eb6e7a86ab8
Wewe <73670886+EwelinaGal@users.noreply.github.com>
2020-11-13 11:23:36 +0100

    Update p1.cpp

--- p1.cpp
+++ p1.cpp
@@ -17,6 +17,7 @@
 
 using namespace std;
 
+
 int make_open_wrfifo(char const *myfifo)
 {
     mkfifo(myfifo, 0666);
@@ -35,52 +36,56 @@ int make_open_rdfifo(char const *myfifo)
     return fd; // zwraca otwarty file descriptor[]
 }
 
+char msg1[100], msg2[100];
+
 int main(int argc, char *argv[], char *envp[])
 {
 
     printf("Beginning of the program \n");
     printf("PID=%d  RODZIC \n", getpid());
 
-    char msg1[10];
-    
-
-    int fd = make_open_wrfifo(argv[1]);
-    
-    //int stdin = make_open_wrfifo(stdin);
-    //int stdout = make_open_rdfifo(stdout);
-
-    // petla sprawdzajaca lacza
-    int maxfds = (fd > STDIN_FILENO) ? fd : STDIN_FILENO;
+    //char msg1[10];
 
     fd_set fds;
+    int fd1 = make_open_rdfifo(argv[1]);
+    int fd2 = make_open_wrfifo(argv[2]);
 
+    cout << msg1 << endl;
+    cout << "fd2" << fd2 << endl;
+
+    //write(fd, msg1, strlen(msg1));
+
+    // petla sprawdzajaca lacza
     while (1)
     {
 
         FD_ZERO(&fds); // set do czyszczenia zestawu
-        FD_SET(fd, &fds);
-        FD_SET (STDIN_FILENO, &fds);
+        FD_SET(fd1, &fds);
+        FD_SET(STDIN_FILENO, &fds);
 
-            
-        int maxfds = (fd > STDIN_FILENO) ? fd : STDIN_FILENO;
+        cout << fd1 << fd2 << endl;
+
+        int maxfds = (fd1 > STDIN_FILENO) ? fd1 : STDIN_FILENO;
 
         // select() = number of max stdin / fds, fd_set if read, fd_set if write, fd_set except, time)
         select(maxfds + 1, &fds, &fds, NULL, NULL);
         {
+            printf("Argument 1: %s \n", msg1);
             // FD_ISSET sprawdza czy fd jest w zestawie &fds
+            if (FD_ISSET(fd1, &fds))
+            {
+                read(fd1, msg1, sizeof(msg1));
+                printf("Message received from fd1: %s \n", msg1);
+            }
+
             if (FD_ISSET(STDIN_FILENO, &fds))
             {
                 read(STDIN_FILENO, msg1, sizeof(msg1));
+
                 printf("Message received from stdin: %s \n", msg1);
             }
-
-            if(FD_ISSET(fd, &fds))
-            {
-                write(fd, msg1, strlen(msg1));
-                printf("Message received from pipe: %s \n", msg1);
-            }
-
-
+            
+            write(fd2, msg1, strlen(msg1));
         }
     }
 
